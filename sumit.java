cdf208c6a48d0e7bb3c64851ced5156c4c3a4efc
sumit <2016.sumit.rohra@ves.ac.in>
2020-05-10 16:50:42 -0700

    first program

---
+++ sumit.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class sumit
+{
+	public static void main(String args[])
+	{
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter the value of x");
+		int x = sc.nextInt();
+		
+		System.out.println("Enter the value of y");
+		int y = sc.nextInt();
+		
+		int z;
+		z = x + y;		
+		
+		System.out.println(z);
+	}
+}
